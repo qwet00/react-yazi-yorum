3b697b40d7367a09137d98134d2e17ce352e7f22
qwet00 <36811940+qwet00@users.noreply.github.com>
2021-05-15 00:01:17 +0300

    asd

--- api/server.js
+++ api/server.js
@@ -569,7 +569,7 @@ const upload = multer({ storage });
 
 
 app.post('/api/images/single-upload', upload.single('file'), async (req, res) => {
-  console.log(imagepath)
+  let imagePath = req.file.path.replace("public",baseURL);
   return res.json({
       imagePath
   });
